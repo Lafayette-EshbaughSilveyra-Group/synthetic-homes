b53829786eb72f069ea401b2972118d51f2a1b13
Jackson Eshbaugh <eshbaugj@lafayette.edu>
2025-07-26 11:41:29 -0400

    remove invalid prototype occlusion test

--- src/experiments/occlusion/__init__.py
+++ src/experiments/occlusion/__init__.py
@@ -1,6 +1,5 @@
-from . import general_occlusion
 from . import roof_occlusion
 from . import llava_roof_occlusion
 from . import run_occlusions
 
-__all__ = ['general_occlusion', 'roof_occlusion', 'llava_roof_occlusion', 'run_occlusions']
+__all__ = ['roof_occlusion', 'llava_roof_occlusion', 'run_occlusions']
--- src/experiments/occlusion/run_occlusions.py
+++ src/experiments/occlusion/run_occlusions.py
@@ -1,9 +1,8 @@
-from experiments.occlusion import general_occlusion, roof_occlusion, llava_roof_occlusion
+from experiments.occlusion import roof_occlusion, llava_roof_occlusion
 
 
 def run_occlusion_suite():
     tests = [
-        ("HVAC Occlusion Tests", general_occlusion.main),
         ("Roof Occlusion Tests", roof_occlusion.main),
         ("LLaVA Occlusion Tests", llava_roof_occlusion.main),
     ]
